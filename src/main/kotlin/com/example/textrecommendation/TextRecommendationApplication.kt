8744cbf2be5a1b92917d5bfabb3c9cd67acd6223
SmiteFLame <lukigreen@naver.com>
2021-12-10 17:39:15 +0900

    init

---
+++ src/main/kotlin/com/example/textrecommendation/TextRecommendationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.textrecommendation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TextRecommendationApplication
+
+fun main(args: Array<String>) {
+	runApplication<TextRecommendationApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/textrecommendation/TextRecommendationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.textrecommendation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TextRecommendationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
